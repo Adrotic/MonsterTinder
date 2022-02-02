40366d9839c351ec332011199d9c680e966b53af
Andrew <andrewclark030@gmail.com>
2022-02-01 19:17:36 -0500

    created unity project (and some scripts)

---
+++ Assets/Scripts/Phone/Phone.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phone : MonoBehaviour
+{
+    public enum PhoneApp
+    {
+        love,
+        text,
+        location
+    }
+
+    public PhoneApp currentApp;
+
+    /*
+    public P_Love appLove;
+    public P_Text appText;
+    public P_Location appLocation;
+    */
+
+    public int service;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
